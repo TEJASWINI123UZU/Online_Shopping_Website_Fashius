4e60e4820e9ee8655676b473af49c41520e22772
Tejaswini <tejaswinisingh023@gmail.com>
2021-02-09 20:28:15 +0530

    fashius

---
+++ online shopping/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+namespace online_shopping
+{
+    public class Class1
+    {
+        public SqlConnection con=new SqlConnection("Data Source=.\\SQLEXPRESS;AttachDbFilename=|DataDirectory|\\Database1.mdf;Integrated Security=True;User Instance=True");
+        public SqlCommand cmd;
+        public SqlCommand cmd1;
+        public SqlDataAdapter da;
+        public SqlDataReader dr;
+
+    }
+}
